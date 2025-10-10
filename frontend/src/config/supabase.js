684d87e715cf9a0dd19445741c86d0093978b1b7
Piyush Panwar <panwarpiyush653@gmail.com>
2025-10-10 20:55:15 +0530

    Updated supabase.js config

--- frontend/src/config/supabase.js
+++ frontend/src/config/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL?.trim();
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY?.trim();
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL or Key is missing. Check your .env file");
