7dca8e004213ba3c57a898fe8535016351e8353e
Piyush Panwar <panwarpiyush653@gmail.com>
2025-10-10 13:28:24 +0530

    Updated frontend axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// Get API URL from Vite environment variable
+const API_URL = import.meta.env.VITE_API_URL;
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000", // your backend URL
+  baseURL: API_URL, // your backend URL (dynamic)
 });
 
 export default axiosInstance;
